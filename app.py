5c775be61da6ccea516e15f784503cf783a62acd
Milene Desjardins <m.desjardins31@outlook.com>
2024-03-05 10:51:49 -0500

    recherche

--- app.py
+++ app.py
@@ -53,23 +53,29 @@ def accueil():
                            utilisateurs=utilisateurs)
 
 
-@app.route('/recherche', methods=['GET', 'POST'])
+@app.route('/recherche', methods=['POST'])
 def recherche():
-    if request.method == 'POST':
-        recherche_input = request.form["recherche_input"]
-        if not recherche_input:
-            return redirect('/'), 302
-        else:
-            db = get_db()
-            articles = db.get_articles(recherche_input)
-            return redirect(url_for('/resultats', query=recherche_input, articles=articles)), 302
-    elif request.method == 'GET':
-        return render_template("index.html")
+    recherche_input = request.form.get("recherche_input")
+    if not recherche_input:
+        # Si aucun terme de recherche n'est saisi, rediriger vers la page de résultats sans terme de recherche
+        return redirect(url_for('resultats')), 302
+    else:
+        # Effectuer la recherche avec le terme saisi par l'utilisateur
+        return redirect(url_for('resultats', query=recherche_input)), 302
 
 
-@app.route('/resultats/<query>', methods=['GET', 'POST'])
+@app.route('/resultats/<query>', methods=['GET'])
+@app.route('/resultats/', defaults={'query': None}, methods=['GET'])
 def resultats(query):
-    articles = request.args.get('articles', [])
+    if query:
+        # Effectuer la recherche en fonction du terme de recherche (query) et récupérer les articles correspondants
+        db = get_db()
+        articles = db.get_articles(query)
+    else:
+        # Si aucun terme de recherche n'est spécifié, récupérer tous les articles
+        db = get_db()
+        articles = db.get_articles()
+
     return render_template('resultats.html', query=query, articles=articles)
 
 
@@ -188,7 +194,6 @@ def articles():
 @app.route('/article/<identifiant>', methods=['GET'])
 def article(identifiant):
     titre = "Article"
-    photo = get_db().get_photo(session["id_photo"])
 
     db = get_db()
     article = db.get_article_by_id(identifiant)  # Récupérer un article par son ID
